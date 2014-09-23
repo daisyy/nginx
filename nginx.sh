9cc8743691e7d315cb9dac28dfbfde0e77d24467
Jackong <JackongC@gmail.com>
2014-09-23 13:13:29 +0800

    update nginx docker info

--- nginx.sh
+++ nginx.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-docker run -t -i -d --name php -v /www/dama:/usr/local/nginx/html/dama:ro -v /var/log/app/dama:/var/log/app/dama -e LISTEN=0.0.0.0:9000 andrei821/docker-php5-fpm
-docker run --name daisyy-nginx --link php:php -v /www/nginx.conf:/etc/nginx.conf:ro -v /www:/usr/local/nginx/html:ro -d -p 80:80 nginx
+docker run --name daisyy-nginx --link dama-php:dama-php -v /www/nginx.conf:/etc/nginx.conf:ro -v /www:/usr/local/nginx/html:ro -d -p 80:80 nginx
