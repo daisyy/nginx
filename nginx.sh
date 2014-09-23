fe3dafd72b3116497096a8c0afaa0a9ee7d963bf
Jackong <JackongC@gmail.com>
2014-09-23 11:10:43 +0800

    update docker script

--- nginx.sh
+++ nginx.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-docker build -t daisy-nginx .
-docker run --name daisyy-nginx -d -p 80:80 daisy-nginx
-#docker run -t -i -d --name php -v /www:/usr/local/nginx/html:ro -e LISTEN=0.0.0.0:9000 andrei821/docker-php5-fpm
-#docker run --name daisyy-nginx --link php:php -v /www/nginx.conf:/etc/nginx.conf:ro -v /www:/usr/local/nginx/html:ro -d -p 80:80 nginx
+docker run -t -i -d --name php -v /www/dama:/usr/local/nginx/html/dama:ro -v /var/log/app/dama:/var/log/app/dama -e LISTEN=0.0.0.0:9000 andrei821/docker-php5-fpm
+docker run --name daisyy-nginx --link php:php -v /www/nginx.conf:/etc/nginx.conf:ro -v /www:/usr/local/nginx/html:ro -d -p 80:80 nginx
