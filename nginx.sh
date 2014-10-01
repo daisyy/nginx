6abfc82d85f3c09b20c46c33b153d74e0dd0644e
Jackong <JackongC@gmail.com>
2014-10-01 16:09:41 +0800

    update nginx config

--- nginx.sh
+++ nginx.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run --name daisyy-nginx --link dama-php:dama-php -v /www/nginx.conf:/etc/nginx.conf:ro -v /www:/usr/local/nginx/html:ro -d -p 80:80 nginx
+docker run --name daisyy-nginx --link dama-php:dama-php --link tools-node:tools-node -v /www/nginx.conf:/etc/nginx.conf:ro -v /www:/usr/local/nginx/html:ro -d -p 80:80 nginx
